9ded6fd2b096574a164c57ee5451d72e354c8db1
Magda Gutowska <m.gutowska0909@gmail.com>
2018-03-18 00:16:30 +0100

    Pole trójkąta w JS

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+var a = prompt('Podaj bok a');
+var	h = prompt('Podaj wysokość h');
+var	triangleArea = a*h/2;
+
+alert("Pole wynosi " + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
